59dda4d1019f46d5c41ad6d9a1fd938316383feb
Nick <nickgomvelez@gmail.com>
2024-11-15 22:12:32 -0500

    style: add styles for the editar_acerca form

--- dashboard_admin/forms.py
+++ dashboard_admin/forms.py
@@ -2,6 +2,14 @@ from django import forms
 from .models import AcercaDe
 
 class AcercaDeForm(forms.ModelForm):
+    mision = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': '3'}))
+    ubicacion = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    instagram = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    facebook = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    telefono = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    whatsapp = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    
+
     class Meta:
         model = AcercaDe
         fields = ['mision', 'ubicacion', 'instagram', 'facebook', 'telefono', 'whatsapp']
