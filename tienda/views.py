530b1d0134bafe2b8e04959329b848b382f3c8bc
Nick <nickgomvelez@gmail.com>
2024-11-27 16:40:07 -0500

    feat: add products to the tienda.html

--- tienda/views.py
+++ tienda/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from dashboard_admin.models import Producto
 # Create your views here.
 
 def tienda_view(request):
-  return render(request, 'tienda.html')
+  productos = Producto.objects.all()
+  return render(request, 'tienda.html', {'productos': productos})
 
 def carrito_view(request):
   return render(request, 'carrito.html')
